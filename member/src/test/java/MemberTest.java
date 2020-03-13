7bfe747d896474f625d59b975b0f05a3a0d37b12
yichuancq <1012027293@qq.com>
2020-03-13 15:45:52 +0800

    添加maven打包配置

--- member/src/test/java/MemberTest.java
+++ member/src/test/java/MemberTest.java
@@ -1,11 +1,11 @@
-
-import member.MemberApp;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MemberApp.class)
-public class MemberTest {
-
-}
+//
+//import member.MemberApp;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = MemberApp.class)
+//public class MemberTest {
+//
+//}
